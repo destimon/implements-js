b323154e276bf6f13ed2bf682164e6fd42d19898
destimon <destaom@gmail.com>
2020-03-30 05:15:29 +0300

    npm init

--- im_promise.js
+++ im_promise.js
@@ -1,37 +1,61 @@
 class im_Promise {
 
-    constructor(callback) {
-        this.data = '';
-        callback(this.resolve, this.reject);
-    }
+  constructor(callback) {
+    this.state = 'pending';
+    this.length = 1;
+    callback(this.resolve, this.reject);
+  }
 
-    resolve = (result) => {
-        this.data = result;
+  resolve = (result) => {
+    if (this.state == 'pending') {
+      this.data = result;
+      this.state = 'fulfilled';
     }
+  }
 
-    reject = (result) => {
-        console.log('reject');
+  reject = (result) => {
+    if (this.state == 'pending') {
+      this.err = result;
+      this.state = 'rejected';
     }
+  }
 
-    then = (callback) => {
-        callback(this.data);
+  then = (callback) => {
+    if (this.state == 'fulfilled') {
+      callback(this.data);
+      return this;
     }
+  }
+
+  catch = (callback) => {
+    if (this.state == 'rejected') {
+      callback(this.err);
+      return this;
+    }
+  }
 }
 
-let prom = new im_Promise(function(resolve, reject) {
-    console.log('out');
-    resolve('data');
-})
-.then(data => {
+(async () => {
+  let prom = new im_Promise(function(resolve, reject) {
+    console.log('out1');
+    reject('error1');
+  })
+  // .then(data => {
+  //   console.log(data);
+  // })
+  .catch(err => {
+    console.log(err);
+  })
+  
+  let prom2 = new Promise(function(resolve, reject) {
+    console.log('out2');
+    reject('error2');
+  })
+  .then(data => {
     console.log(data);
-})
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})();
 
-console.log('')
-
-let prom2 = new Promise(function(resolve, reject) {
-    console.log('out');
-    resolve('data');
-})
-.then(data => {
-    console.log(data);
-})
